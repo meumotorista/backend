52463f451ed9821c26e576a081d19b5a29d4d505
Wagner Mobile Costa <mobile.costa@gmail.com>
2026-01-16 22:47:54 -0300

    remoção do projeto

--- src/controllers/rideController.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-export declare class RideController {
-    create(req: Request, res: Response): Promise<void>;
-    accept(req: Request, res: Response): Promise<void>;
-    getStatus(req: Request, res: Response): Promise<void>;
-}
-//# sourceMappingURL=rideController.d.ts.map
